c156256564bc6ebc5fea80e0bdde04f4f8aefe69
wangxiaoyu <572710757@qq.com>
2022-05-17 10:40:45 +0800

    first commit

---
+++ src/GitTest.java
@@ -0,0 +1,10 @@
+/**
+ * @author WXYstart
+ * @discription hello
+ * @time 2022/5/17
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
